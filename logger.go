b894d239089442863de74cb9dade93d782411bce
Benjamin Drung <bdrung@debian.org>
2020-12-27 01:31:39 +0100

    hacky BME280 support
    Signed-off-by: Benjamin Drung <bdrung@debian.org>

---
+++ logger.go
@@ -0,0 +1,5 @@
+package main
+
+import logger "github.com/d2r2/go-logger"
+
+var lg = logger.NewPackageLogger("sensor", logger.InfoLevel)
--- prometheus-sensor-exporter.go
+++ prometheus-sensor-exporter.go
@@ -8,7 +8,9 @@ import (
 	"strings"
 	"sync"
 
+	bsbmp "github.com/d2r2/go-bsbmp"
 	i2c "github.com/d2r2/go-i2c"
+	logger "github.com/d2r2/go-logger"
 	sht3x "github.com/d2r2/go-sht3x"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -25,6 +27,14 @@ type Sensor struct {
 	mutex   sync.Mutex
 }
 
+type BME280Sensor struct {
+	Address uint8
+	Bus     int
+	Model   string
+	bme     *bsbmp.BMP
+	mutex   sync.Mutex
+}
+
 func NewSensor(address uint8, bus int, model string) *Sensor {
 	fmt.Printf("New sensor: %s,address=0x%x,bus=%d\n", model, address, bus)
 	i2c, err := i2c.NewI2C(address, bus)
@@ -37,6 +47,29 @@ func NewSensor(address uint8, bus int, model string) *Sensor {
 		Model:   model,
 		I2C:     i2c,
 		SHT3X:   *sht3x.NewSHT3X(),
+	}
+}
+
+func NewBME280Sensor(address uint8, bus int, model string) *BME280Sensor {
+	fmt.Printf("New sensor: %s,address=0x%x,bus=%d\n", model, address, bus)
+
+	// todo loglevel flag
+	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
+	logger.ChangePackageLogLevel("bsbmp", logger.InfoLevel)
+
+	i2c, err := i2c.NewI2C(address, bus)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bme, err := bsbmp.NewBMP(bsbmp.BME280, i2c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &BME280Sensor{
+		Address: address,
+		Bus:     bus,
+		Model:   model,
+		bme:     bme,
 	}
 }
 
@@ -62,8 +95,38 @@ func SensorFromMap(model string, fields map[string]string) *Sensor {
 	return NewSensor(address8, bus, model)
 }
 
+func BME280SensorFromMap(model string, fields map[string]string) *BME280Sensor {
+	// Defaults
+	var address8 uint8 = 0x76
+	var bus int = 0
+
+	if address, ok := fields["address"]; ok {
+		address64, _ := strconv.ParseUint(address, 0, 8)
+		address8 = uint8(address64)
+	} else {
+		log.Println("unknown address:", address)
+	}
+
+	if bus_str, ok := fields["bus"]; ok {
+		bus32, _ := strconv.ParseInt(bus_str, 0, 32)
+		bus = int(bus32)
+	} else {
+		log.Println("unknown bus:", bus_str)
+	}
+
+	return NewBME280Sensor(address8, bus, model)
+}
+
 type sensorCollector struct {
 	Sensor       *Sensor
+	Up           *prometheus.Desc
+	TemperatureC *prometheus.Desc
+	HumidityRH   *prometheus.Desc
+	HumidityGram *prometheus.Desc
+}
+
+type BME280SensorCollector struct {
+	Sensor       *BME280Sensor
 	Up           *prometheus.Desc
 	TemperatureC *prometheus.Desc
 	HumidityRH   *prometheus.Desc
@@ -97,7 +160,41 @@ func NewSensorCollector(c *Sensor) *sensorCollector {
 	}
 }
 
+func NewBME280SensorCollector(c *BME280Sensor) *BME280SensorCollector {
+	labels := prometheus.Labels{"address": fmt.Sprintf("0x%x", c.Address), "bus": fmt.Sprintf("%d", c.Bus), "model": c.Model}
+	return &BME280SensorCollector{
+		Sensor: c,
+		TemperatureC: prometheus.NewDesc("sensor_temperature_celsius",
+			"The temperature in Celsius",
+			nil,
+			labels,
+		),
+		HumidityRH: prometheus.NewDesc("sensor_humidity_percent",
+			"Relative humidity in percent",
+			nil,
+			labels,
+		),
+		HumidityGram: prometheus.NewDesc("sensor_humidity_grams_per_cubic_meter",
+			"Absolute humidity in gram / cubic meter",
+			nil,
+			labels,
+		),
+		Up: prometheus.NewDesc("sensor_up",
+			"TODO",
+			nil,
+			labels,
+		),
+	}
+}
+
 func (collector *sensorCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collector.TemperatureC
+	ch <- collector.HumidityRH
+	ch <- collector.HumidityGram
+	ch <- collector.Up
+}
+
+func (collector *BME280SensorCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.TemperatureC
 	ch <- collector.HumidityRH
 	ch <- collector.HumidityGram
@@ -139,6 +236,51 @@ func (collector *sensorCollector) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(collector.HumidityGram, prometheus.GaugeValue, Relative2AbsoluteHumidity(temp2, rh2))
 }
 
+func (collector *BME280SensorCollector) Collect(ch chan<- prometheus.Metric) {
+	var temp, rh float32
+	var err error
+	//if collector.Sensor.Bus == 0 {
+	//	temp = 20.0
+	//	rh = 50.0
+	//	time.Sleep(100 * time.Millisecond)
+	//	err = errors.New("prometheus-sensor-exporter: Fake failure")
+	//} else {
+	collector.Sensor.mutex.Lock()
+	temp, err = collector.Sensor.bme.ReadTemperatureC(bsbmp.ACCURACY_STANDARD)
+	collector.Sensor.mutex.Unlock()
+	//}
+	if err != nil {
+		log.Print(err)
+		ch <- prometheus.MustNewConstMetric(collector.Up, prometheus.GaugeValue, 0.0)
+		return
+	}
+
+	var temp2, rh2 float64
+	temp2 = round64(float64(temp), 2)
+	ch <- prometheus.MustNewConstMetric(collector.TemperatureC, prometheus.GaugeValue, temp2)
+
+	collector.Sensor.mutex.Lock()
+	_, rh, err = collector.Sensor.bme.ReadHumidityRH(bsbmp.ACCURACY_STANDARD)
+	collector.Sensor.mutex.Unlock()
+	//}
+	if err != nil {
+		log.Print(err)
+		ch <- prometheus.MustNewConstMetric(collector.Up, prometheus.GaugeValue, 0.0)
+		return
+	}
+
+	rh2 = round64(float64(rh), 2)
+
+	ch <- prometheus.MustNewConstMetric(collector.HumidityRH, prometheus.GaugeValue, rh2)
+	ch <- prometheus.MustNewConstMetric(collector.Up, prometheus.GaugeValue, 1.0)
+	ch <- prometheus.MustNewConstMetric(collector.HumidityGram, prometheus.GaugeValue, Relative2AbsoluteHumidity(temp2, rh2))
+
+	// TODO:
+	// sensor.ReadPressurePa
+	// sensor.ReadPressureMmHg
+	// sensor.ReadAltitude
+}
+
 func main() {
 	listenAddress := flag.String(
 		"web.listen-address", ":9775", "Address on which to expose metrics and web interface.",
@@ -173,6 +315,10 @@ func main() {
 		case "SHT35":
 			sensor := SensorFromMap(model, m)
 			collector := NewSensorCollector(sensor)
+			prometheus.MustRegister(collector)
+		case "BME280":
+			sensor := BME280SensorFromMap(model, m)
+			collector := NewBME280SensorCollector(sensor)
 			prometheus.MustRegister(collector)
 		default:
 			log.Fatal("Invalid model '%s'!", model)
